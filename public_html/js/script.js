09f6a2e7c400fe2b1fa596cabbd6728d9961775b
Stevo5o <stevo.joc@gmail.com>
2015-02-22 21:14:44 +0000

    add sweetalet pop-up and AJAX JSON

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,12 +1,46 @@
-document.querySelector('.grid').addEventListener('click', function (e) {
-    if (e.target.tagName === 'IMG') {
-        var howmany = this.querySelectorAll('li').lenght;
-        if (howmany > 1) {
-            var removeTarget = e.target.parentNode;
-            removeTarget.parentNode.removeChild(removeTarget);
-        } else {
-            var projectName = e.target.alt;
-            document.querySelector('#projects p').innerHTML = "<p>" + projectName + "</p>";
-        } // howmany
-    } // check to see that I clicked on IMG only
-}, false); // click event
+/**
+ * myscript.js Projects Pop-up
+ * @ Stephen O'Connor, Feburary 2015
+ * 
+ * Dependencies:
+ * https://developers.google.com/speed/libraries/devguide#jquery
+ * http://tristanedwards.me/sweetalert 
+ * 
+ */
+
+// immediately invoked anonymous function
+(function () {
+
+    // jQuery AJAX call
+    $.ajax({
+        url: 'data.json',
+        dataType: 'json',
+        type: 'get',
+        cache: false,
+        success: function (data) {
+            document.querySelector('.grid').addEventListener('click', function (e) {
+                console.log(e.target.id);
+                if (e.target.tagName == 'IMG') {
+
+                    var projectName = e.target.alt;
+                    for (var i = 0; i < data.projects.length; i++) {
+                        console.log(data.projects[i].alt);
+                        if (e.target.alt === data.projects[i].alt) {
+
+                            swal(
+                                    "Details", "Name: " +
+                                    data.projects[i].name +
+                                    "\n Company: " +
+                                    data.projects[i].companyName +
+                                    "\nJob title: " +
+                                    data.projects[i].jobTitle +
+                                    "\n Email: " +
+                                    data.projects[i].emailAdd);
+                        }
+                    }
+                }
+
+            }, false);
+        }
+    });
+}()); // end immediately invoked anonymous function
