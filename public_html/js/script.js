76fd4dff91c4089b67ea2d8c0fb6e3b011c08bb6
Stevo5o <stevo.joc@gmail.com>
2015-02-22 22:49:12 +0000

    edit JSON and swal

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -20,27 +20,42 @@
         success: function (data) {
             document.querySelector('.grid').addEventListener('click', function (e) {
                 console.log(e.target.id);
-                if (e.target.tagName == 'IMG') {
+                if (e.target.tagName === 'IMG') {
+                    console.log(e.target.alt);
 
-                    var projectName = e.target.alt;
                     for (var i = 0; i < data.projects.length; i++) {
-                        console.log(data.projects[i].alt);
-                        if (e.target.alt === data.projects[i].alt) {
 
-                            swal(
-                                    "Details", "Name: " +
-                                    data.projects[i].name +
-                                    "\n Company: " +
-                                    data.projects[i].companyName +
-                                    "\nJob title: " +
-                                    data.projects[i].jobTitle +
-                                    "\n Email: " +
-                                    data.projects[i].emailAdd);
+                        if (e.target.alt === data.projects[i].alt) {
+                            swal({title: "Project: " + data.projects[i].companyName, text: "You will not be able to recover this imaginary file!", type: "warning", showCancelButton: true, confirmButtonColor: "#DD6B55", confirmButtonText: "Yes, delete it!", cancelButtonText: "No, cancel plx!", closeOnConfirm: false, closeOnCancel: false}, function (isConfirm) {
+                                if (isConfirm) {
+                                    swal("Deleted!", "Your imaginary file has been deleted.", "success");
+                                } else {
+                                    swal("Cancelled", "Your imaginary file is safe :)", "error");
+                                }
+                            });
+
+
+                            //                     swal({
+                            //                             "Details", "Company: " +
+                            //                             data.projects[i].companyName +
+                            //                             "\nJob title: " +
+                            //                             data.projects[i].jobTitle +
+                            //                             "\n Link: " +
+                            //                              data.projects[i].link 
+                            //                              //+ 
+                            // //                     "text", "By clicking 'OK' you will be redirected to the link." +
+                            // //                            "type," warning + 
+                            // // "showCancelButton", true
+                            // });
+
                         }
                     }
                 }
-
             }, false);
         }
     });
 }()); // end immediately invoked anonymous function
+
+// title: "Are you sure?",
+//        
+ 
